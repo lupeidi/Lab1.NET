d297bc91de93cde8a3be91bf241feb1145e617e0
Diana <lupeidi@gmail.com>
2020-06-12 20:48:39 +0300

    Add project files.

---
+++ Laborator1/Controllers/BookController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laborator1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime;
+
+namespace Laborator1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookContext _context;
+
+        public BookController(BookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Book
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Book.ToListAsync();
+        }
+
+        // GET: Book/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(long id)
+        {
+            var book = await _context.Book.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+
+
+            return book;
+        }
+
+        // PUT: Book/id
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(long id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            if (!BookExists(id))
+            {
+                return NotFound();
+            }
+            _context.Entry(book).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        // POST: Book
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Book.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/Book/id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> DeleteBook(long id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Book.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        private bool BookExists(long id)
+        {
+            return _context.Book.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Laborator1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laborator1.Models
+{
+    public class Book
+    {
+        public long Id { get; set;  }
+        public string Title { get; set; }
+        public int PublishingYear { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
